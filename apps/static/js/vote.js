17e680990a132a23396619a283848ba8adbfbcc4
chaminjae <beingeskimo@gmail.com>
2020-12-21 14:24:34 +0900

    commit

--- apps/static/js/vote.js
+++ apps/static/js/vote.js
@@ -1,10 +1,12 @@
 function chkFormNull() {
   var len = document.voteForm.candidate.length;
   var flag;
+  var chked;
 
   for (i = 0; i < len; i++) {
     if (document.voteForm.candidate[i].checked) {
       flag = true;
+      chked = document.voteForm.candidate[i].value;
       break;
     } else {
       flag = false;
@@ -15,6 +17,9 @@ function chkFormNull() {
     alert("후보자를 선택하세요.");
     return false;
   } else {
-    document.voteForm.submit();
+    var conf = confirm(chked + " 번 후베에게 투표하시겠습니까?");
+    if (conf) {
+      document.voteForm.submit();
+    }
   }
 }
